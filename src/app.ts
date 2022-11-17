51682beea95bae32d307955ae3d2d9aabd0f238c
Gaston Morixe <gaston@gastonmorixe.com>
2022-11-17 04:46:56 -0300

    working

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,9 @@
 import noble from "@abandonware/noble"
+import argv from "minimist"
+
+const args = argv(process.argv.slice(2))
+
+console.log("args", args)
 
 enum LEDCommands {
   Blue = "7BFF0700FF00FFFFBF",
@@ -36,26 +41,43 @@ noble.on("discover", async (peripheral) => {
       console.log("characteristic: ", c.name)
       try {
         console.log("writting...", { c })
-        const data = Buffer.from(LEDCommands.On, "hex")
-        c.writeAsync(data, true)
+
+        let cmd: LEDCommands = LEDCommands.On
+        if (args._.includes("off")) {
+          console.log("OFF !!!!", { c })
+          cmd = LEDCommands.Off
+        }
+        const data = Buffer.from(cmd, "hex")
+        const write = await c.writeAsync(data, true)
+        console.log("write:", write)
       } catch (err) {
         console.log("writting erro")
         console.error(err)
       }
     }
+
+    // process.exit(0)
+
     console.log("characteristics: ", characteristics)
     // const batteryLevel = (await characteristics[0].readAsync())[0]
 
     console.log(`${peripheral.address} (${peripheral.advertisement.localName})`)
 
-    // try {
-    //   const dis = await peripheral.disconnectAsync()
-    //   console.log(dis)
-    // } catch (e) {
-    //   console.error(e)
-    // }
-    // process.exit(0)
+    // setTimeout(async () => {
+    //   try {
+    //     const dis = await peripheral.disconnectAsync()
+    //     console.log("dis: ", dis)
+    //   } catch (e) {
+    //     console.log("disc error")
+    //     console.error(e)
+    //   }
+    // }, 5000)
   } catch (e) {
+    console.log("error end")
     console.error(e)
   }
+
+  setTimeout(() => {
+    process.exit(0)
+  }, 5000)
 })
